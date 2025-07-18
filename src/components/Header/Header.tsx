512499001159fe423c93bcabc087c88859b99b2d
Stepan Dordiai <stepandordiai@gmail.com>
2025-07-18 08:26:24 +0200

    Update Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,31 +11,31 @@ const Header = () => {
 
 	const { lng } = useParams();
 
-	useEffect(() => {
-		let lastScrollTop = 0;
+	// useEffect(() => {
+	// 	let lastScrollTop = 0;
 
-		function handleHeaderOnScroll(): void {
-			const header = document.querySelector(".header") as HTMLElement | null;
-			let scrollTop = document.documentElement.scrollTop;
-			if (scrollTop > lastScrollTop && scrollTop > window.innerHeight) {
-				header?.classList.add("header--hide");
-			} else {
-				header?.classList.remove("header--hide");
-			}
-			lastScrollTop = scrollTop;
-		}
+	// 	function handleHeaderOnScroll(): void {
+	// 		const header = document.querySelector(".header") as HTMLElement | null;
+	// 		let scrollTop = document.documentElement.scrollTop;
+	// 		if (scrollTop > lastScrollTop && scrollTop > window.innerHeight) {
+	// 			header?.classList.add("header--hide");
+	// 		} else {
+	// 			header?.classList.remove("header--hide");
+	// 		}
+	// 		lastScrollTop = scrollTop;
+	// 	}
 
-		document.addEventListener("scroll", handleHeaderOnScroll);
+	// 	document.addEventListener("scroll", handleHeaderOnScroll);
 
-		return () => {
-			document.removeEventListener("scroll", handleHeaderOnScroll);
-		};
-	}, []);
+	// 	return () => {
+	// 		document.removeEventListener("scroll", handleHeaderOnScroll);
+	// 	};
+	// }, []);
 
 	return (
 		<header className="header">
 			<NavLink to={`/${lng}`} className="header__logo">
-				<img width={30} src={logo} alt="" />
+				<img width={30} src={logo} alt="Yagoda Karpat Logo" />
 				<span>{t("company_name")}</span>
 			</NavLink>
 			<LngSelect />
