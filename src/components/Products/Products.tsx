cba3a4ffa7c0a1054a59748064dc2b01b64129af
Stepan Dordiai <stepandordiai@gmail.com>
2025-09-03 17:43:06 +0200

    Update Products and data

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -32,16 +32,66 @@ const Products: React.FC<ProductsProps> = ({ productsData }) => {
 	const { t } = useTranslation();
 
 	const [search, setSearch] = useState("");
+	const [productType, setProductType] = useState("all");
 
 	function clearSearch() {
 		setSearch("");
 	}
+
+	const uniqueTypes = [...new Set(productsData.map((product) => product.type))];
+
+	const handleProductType = (props: string) => {
+		setProductType((prev) => (prev = props));
+	};
+
+	// TODO:
+	const filteredProducts = productsData.filter((product) => {
+		// const matchesSearch = search.trim() === ""
+		// 					? product
+		// 					: // I used startsWith method here, but i think includes method is much better here.
+		// 					  // For example with 'black currant', if i use startsWith, i have to start with black to find this product.
+		// 					  t(product.name).toLowerCase().includes(search.toLowerCase());
+
+		const matchesSearch =
+			search.trim() === "" ||
+			t(product.name).toLowerCase().includes(search.toLowerCase());
+
+		const matchesType = productType === "all" || product.type === productType;
+
+		return matchesSearch && matchesType;
+	});
 
 	return (
 		<div className="js-products" id="products">
 			<SectionTitle name={t("products_title")} />
 			<div>
 				<p className="filter-title">{t("products.filter")}</p>
+				<div className="products__btn-container">
+					<button
+						onClick={() => handleProductType("all")}
+						className={
+							productType === "all"
+								? "products__btn products__btn--active"
+								: "products__btn"
+						}
+					>
+						{t("all")}
+					</button>
+					{uniqueTypes.map((type) => {
+						return (
+							<button
+								onClick={() => handleProductType(type)}
+								className={
+									productType === type
+										? "products__btn products__btn--active"
+										: "products__btn"
+								}
+							>
+								{t(type)}
+							</button>
+						);
+					})}
+				</div>
 				<div className="search-wrapper">
 					<input
 						className="search-input"
@@ -63,17 +113,9 @@ const Products: React.FC<ProductsProps> = ({ productsData }) => {
 				</div>
 			</div>
 			<div className="products-container">
-				{productsData
-					.filter((product) => {
-						return search.trim() === ""
-							? product
-							: // I used startsWith method here, but i think includes method is much better here.
-							  // For example with 'black currant', if i use startsWith, i have to start with black to find this product.
-							  t(product.name).toLowerCase().includes(search.toLowerCase());
-					})
-					.map((product) => {
-						return <Product key={product.id} product={product} />;
-					})}
+				{filteredProducts.map((product) => {
+					return <Product key={product.id} product={product} />;
+				})}
 			</div>
 		</div>
 	);
