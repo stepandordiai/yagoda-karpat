2c9cc55e7d6a8040edc34e57e73dc0a02f6775b4
Stepan Dordiai <stepandordiai@gmail.com>
2025-10-31 08:16:34 +0100

    Update Contacts

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -16,7 +16,7 @@ const Contacts = () => {
 			<div className="form-map-container">
 				<ContactForm />
 				<div className="contacts__map-wrapper">
-					<h2 className="contacts__map-title">{t("contacts.map_title")}</h2>
+					<h3 className="contacts__map-title">{t("contacts.map_title")}</h3>
 					<iframe
 						className="contacts__map"
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2664.3077328162444!2d23.7462056!3d48.1042991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47379f0033ca7d7d%3A0xf3d79b73a73bbce3!2z0K_Qs9C-0LTQsCDQmtCw0YDQv9Cw0YIg0KLQntCS!5e0!3m2!1sen!2scz!4v1746348694187!5m2!1sen!2scz"
