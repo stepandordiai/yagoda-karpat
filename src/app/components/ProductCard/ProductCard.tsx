76e43251b54d10b48e5ff30054084af724e41bb9
Stepan Dordiai <stepandordiai@gmail.com>
2026-02-16 21:34:29 +0100

    refactor: update ProductCard component

--- src/app/components/ProductCard/ProductCard.tsx
+++ src/app/components/ProductCard/ProductCard.tsx
@@ -31,69 +31,26 @@ export default function ProductCard({ product }: ProductProps) {
 
 	return (
 		<div className="product">
-			<div className="product-top">
-				<div className="product__img-wrapper">
-					{productState?.images[0] && (
-						<Image
-							className="product__img"
-							src={productState.images[0]}
-							width={1600}
-							height={1200}
-							alt={`${t(name)} ${
-								productState?.state ? t(productState.state) : ""
-							}`.trimEnd()}
-						/>
-					)}
-					<div className="img-qty">
-						<CameraIcon />
-						<span>{productState?.images.length}</span>
-					</div>
-				</div>
-				<div className="product__info-container">
-					<div className="product__info-container-top">
-						<h3 className="product__name">{t(name)}</h3>
-						<h4 style={{ color: "hsl(0, 0%, 50%)" }}>
-							{t("certificates_title")}
-						</h4>
-						<div style={{ display: "flex", columnGap: 5 }}>
-							{isOrganic && (
-								<img
-									src="/icons/organic-logo.jpg"
-									alt="Organic Standard logo"
-									loading="lazy"
-								/>
-							)}
-							<span>HACCP</span>
-						</div>
-						<h4 style={{ color: "hsl(0, 0%, 50%)" }}>
-							{t("product_page.origin_title")}
-						</h4>
-						<p>{t("product_page.origin.region.title")}</p>
-						<h4 style={{ color: "hsl(0, 0%, 50%)" }}>
-							{t("product_page.packaging_title")}
-						</h4>
-						{product.packaging.paperBag && (
-							<p>{t(product.packaging.paperBag.title)}</p>
-						)}
-						{product.packaging.cartonBox && (
-							<p>{t(product.packaging.cartonBox.title)}</p>
-						)}
-						{product.packaging.octabin && (
-							<p>{t(product.packaging.octabin.title)}</p>
-						)}
-						{product.packaging.woodenContainer && (
-							<p>{t(product.packaging.woodenContainer.title)}</p>
-						)}
-					</div>
+			<div className="product__img-wrapper">
+				{productState?.images[0] && (
+					<Image
+						className="product__img"
+						src={productState.images[0]}
+						width={1600}
+						height={1200}
+						alt={`${t(name)} ${
+							productState?.state ? t(productState.state) : ""
+						}`.trimEnd()}
+					/>
+				)}
+				<div className="img-qty">
+					<CameraIcon />
+					<span>{productState?.images.length}</span>
 				</div>
 			</div>
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					gap: 5,
-				}}
-			>
+			<div className="product__info-container">
+				<p style={{ color: "hsl(0, 0%, 50%)" }}>{product.latName}</p>
+				<h3 className="product__name">{t(name)}</h3>
 				{variants.some((variant) => variant.state) && (
 					<div>
 						<h4 style={{ color: "hsl(0, 0%, 50%)", marginBottom: 5 }}>
