ad6840836f4f868e2c0647cbe3fa79546e325012
Stepan Dordiai <stepandordiai@gmail.com>
2026-05-01 16:49:00 +0200

    feat: update Gallery component

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -3,14 +3,16 @@
 import { useTranslations } from "next-intl";
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 import "swiper/css";
-// import { Autoplay } from "swiper/modules";
-import "./Gallery.scss";
 import Image from "next/image";
+import "./Gallery.scss";
 
-const galleryData = [
-	{ img: "/gallery/01-c.jpg", date: "26/11/24" },
-	{ img: "/gallery/02-c.jpg", date: "8/7/24" },
+const gallery = [
+	"/gallery/01-c.jpg",
+	"/gallery/02-c.jpg",
+	"/gallery/03-c.jpeg",
+	"/gallery/04-c.jpeg",
 ];
 
 const Gallery = () => {
@@ -24,23 +26,42 @@ const Gallery = () => {
 					900: {
 						slidesPerView: 4,
 						spaceBetween: 25,
+						loop: false,
+					},
+					600: {
+						slidesPerView: 3,
+						spaceBetween: 10,
+						loop: true,
+					},
+					400: {
+						slidesPerView: 2,
+						spaceBetween: 10,
+						loop: true,
 					},
 				}}
 				spaceBetween={10}
-				slidesPerView={2}
-				// autoplay={{
-				// 	delay: 3000,
-				// 	disableOnInteraction: false,
-				// }}
-				// speed={1000}
-				// loop={galleryData.length > 1}
-				// modules={[Autoplay]}
-				className="mySwiper"
+				slidesPerView={1}
+				autoplay={{
+					delay: 3000,
+					disableOnInteraction: false,
+				}}
+				speed={1000}
+				loop={gallery.length > 1}
+				modules={[Autoplay]}
 			>
-				{galleryData.map(({ img, date }, index) => {
+				{gallery.map((src, index) => {
 					return (
-						<SwiperSlide key={index} className="swiper-card" data-date={date}>
-							<Image src={img} width={448.75} height={598.33} alt="" />
+						<SwiperSlide
+							key={index}
+							className="swiper-card"
+							data-label={t(`gallery.${index}`)}
+						>
+							<Image
+								src={src}
+								width={448.75}
+								height={598.33}
+								alt={t(`gallery.${index}`)}
+							/>
 						</SwiperSlide>
 					);
 				})}
